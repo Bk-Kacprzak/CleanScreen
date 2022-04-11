b107fba73a297cd07c0146db28c6e0277608139d
Bartlomiej Kacprzak <bk.kacprzak@gmail.com>
2022-04-11 12:59:14 +0200

    Add inheritance to class KeyListener

--- sample/key_listener.py
+++ sample/key_listener.py
@@ -2,30 +2,33 @@ from pynput.keyboard import Key, Listener
 from shortcuts import shortcuts
 from active_window import application
 
-class KeyListener :
+class KeyListener(Listener) :
+    "Class responsible for hotkey listening"
     KEY_COMBINATION = set()
-
     def __init__(self) : 
-        self.listener = Listener(self.on_press, self.on_release) 
-        self.listener.start()
-        self.listener.join()
-    @staticmethod
-    def on_press(key) -> bool :
+        super().__init__(
+            on_press = self.on_press,
+            on_release=  self.on_release,
+            suppress = False)
+
+        self.start()
+        self.join()
+
+    def on_press(self,key) -> bool :
         if not isinstance(key, Key) : 
             return
         key_string = str(key)
+
         KeyListener.KEY_COMBINATION.add(key_string)
-        print(KeyListener.KEY_COMBINATION)
- 
-        "Check if specific combination exist"
+        # Check if specific combination exist
+       
         # WARNING : issubset is used to prevent from unremoved cmd_r / ctrl_r from the key_combination
         for shortcut in shortcuts: 
             if shortcut.combination.issubset(KeyListener.KEY_COMBINATION) :
                 application.resize(shortcut.window) 
 
-            
-    @staticmethod 
-    def on_release(key) : 
+
+    def on_release(self,key) : 
         key_string = str(key)
         try:
             KeyListener.KEY_COMBINATION.remove(key_string)
