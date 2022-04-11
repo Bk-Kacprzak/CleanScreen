fb3200e7296f640fbf0017d1c5dff563e1554912
Bartlomiej Kacprzak <bk.kacprzak@gmail.com>
2022-04-11 12:58:10 +0200

    Add windowDimensionRatio object

--- sample/application_window.py
+++ sample/application_window.py
@@ -1,4 +1,5 @@
 from screen import screens, Screen
+from action_manager import WindowDimensionsRatio
 
 class ApplicationRect:
 
@@ -18,31 +19,31 @@ class ApplicationRect:
         top_left_screen = Screen.get_top_left_screen(screens)
 
         for screen in screens: 
-            if screen.origin_x <= self.x < screen.origin_x + screen.size[0]: 
+            if screen.origin_x <= self.x < screen.origin_x + screen.width: 
                 return screens.index(screen)
             
             if self.x < top_left_screen.origin_x : 
                 return screens.index(top_left_screen)
 
     @staticmethod 
-    def count_window_size(x_ratio, y_ratio, w_ratio, h_ratio) : 
+    def count_window_size(windowDimension : WindowDimensionsRatio) : 
         def count_size(screen_index) : 
             # TODO: refactor function using another coordinate system written before in ./screen.py
-            if x_ratio == 0 :
+            if windowDimension.x_ratio == 0 :
                 x = screens[screen_index].origin_x
             else:
-                if screens[screen_index].origin_x + screens[screen_index].size[0] == 0 : 
+                if screens[screen_index].origin_x + screens[screen_index].width == 0 : 
                     "this is first left screen next to main screen"
                     x = screens[screen_index].origin_x
-                    x *= (1-x_ratio)
+                    x *= (1-windowDimension.x_ratio)
                 else : 
-                    x = screens[screen_index].origin_x + screens[screen_index].size[0] * x_ratio
+                    x = screens[screen_index].origin_x + screens[screen_index].width * windowDimension.x_ratio
 
-            y = screens[screen_index].origin_y + screens[screen_index].size[1]
-            y *= y_ratio
+            y = screens[screen_index].origin_y + screens[screen_index].height
+            y *= windowDimension.y_ratio
 
-            width = screens[screen_index].size[0] * w_ratio
-            height = screens[screen_index].size[1] * h_ratio
+            width = screens[screen_index].width * windowDimension.w_ratio
+            height = screens[screen_index].height * windowDimension.h_ratio
 
             return ApplicationRect(x, y, width, height)
 
